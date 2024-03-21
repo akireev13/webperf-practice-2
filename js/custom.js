699941f88fe09e4da23b3e6f2b6534124a07d49b
akireev13 <akireev13@mail.ru>
2024-03-21 11:56:35 +0100

    first

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,14 @@
+import { onLCP, onFID, onCLS } from "web-vitals";
 
-  (function ($) {
-  
+onCLS(console.log);
+onFID(console.log);
+onLCP(console.log);
+
+(function ($) {
   "use strict";
 
-    // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-    
-  })(window.jQuery);
+  // NAVBAR
+  $(".navbar-nav .nav-link").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+})(window.jQuery);
