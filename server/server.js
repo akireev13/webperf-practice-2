1d33b82abcba5fcb393024a6145b2aa8ad93e0f2
akireev13 <akireev13@mail.ru>
2024-03-25 12:18:29 +0100

    added server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const compression = require("compression");
+
+const app = express();
+const port = 5173;
+
+// Add ETags middleware
+app.set("etag", true);
+
+// Enable compression middleware
+app.use(compression());
+
+// Serve static files from the parent directory
+app.use(express.static(".."));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
