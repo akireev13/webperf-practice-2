cc0c7f841eba6f4c3962153555444eaca83308f1
akireev13 <akireev13@mail.ru>
2024-03-28 11:38:38 +0100

    added missing pages, fixed routing

--- my-app/pages/_document.tsx
+++ my-app/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import NavBar from "@/components/NavBar";
 export default function Document() {
   return (
     <Html lang="en">
@@ -15,6 +15,7 @@ export default function Document() {
     />
       </Head>
       <body>
+        <NavBar/>
         <Main />
         <NextScript />
       </body>
--- my-app/pages/coctails/index.tsx
+++ my-app/pages/coctails/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Coctails() {
-    return (<>Coctails</>)
+    return (<>COCTAILS</>)
 }
